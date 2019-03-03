2d84e7337d161540fa1941db67ca6cab59ee8307
jotnguyen <johntnguyen9@gmail.com>
2019-03-03 11:27:44 -0600

    Pulled from master and add fetchSubjects effect

--- src/effects/browse.js
+++ src/effects/browse.js
@@ -4,9 +4,11 @@ import { CURRENT_TERM } from 'util/constants';
 const currentTermDoc = db.collection('terms').doc(CURRENT_TERM);
 
 /* eslint-disable import/prefer-default-export */
-export function fetchSchools() {
+export function fetchSubjects(termId, schoolId) {
   return currentTermDoc
-    .collection('schools')
+    .collection('subjects')
+    .where('termId', '==', termId)
+    .where('schoolId', '==', schoolId)
     .get()
     .then(
       querySnapshot => querySnapshot.docs.map(doc => doc.data()),
